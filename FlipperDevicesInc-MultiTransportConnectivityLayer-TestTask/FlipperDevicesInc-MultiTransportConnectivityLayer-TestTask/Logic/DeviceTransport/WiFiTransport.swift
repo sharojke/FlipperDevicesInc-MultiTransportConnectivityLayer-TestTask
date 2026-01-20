8e1606442915acb301c2553185ade0129755fda3
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2026-01-20 22:01:12 +0300

    Add `WiFiTransport`

---
+++ FlipperDevicesInc-MultiTransportConnectivityLayer-TestTask/FlipperDevicesInc-MultiTransportConnectivityLayer-TestTask/Logic/DeviceTransport/WiFiTransport.swift
@@ -0,0 +1,106 @@
+import Foundation
+
+private enum SupportedEndpoint: CaseIterable {
+    case deviceInfo
+    
+    var rawValue: String {
+        switch self {
+        case .deviceInfo: DeviceRequestEndpoint.deviceInfo
+        }
+    }
+    
+    init?(endpoint: String) {
+        let endpoint = Self.allCases.first { endpoint == $0.rawValue }
+        guard let endpoint else { return nil }
+        
+        self = endpoint
+    }
+}
+
+final class WiFiTransport: AnyDeviceTransport {
+    private let connectionStateManager: AnyDeviceTransportConnectionStateManager
+    private let mockDeviceInfo: DeviceInfo
+    private let connectsSuccessfully: Bool
+    private let sendsRequestSuccessfully: Bool
+    
+    var isAvailable: Bool {
+        get async {
+            try? await Task.shortSleep()
+            return true
+        }
+    }
+    
+    var connectionStateStream: AsyncStream<ConnectionState> {
+        connectionStateManager.connectionStateStream
+    }
+    
+    private init(
+        connectionStateManager: AnyDeviceTransportConnectionStateManager,
+        mockDeviceInfo: DeviceInfo,
+        connectsSuccessfully: Bool,
+        sendsRequestSuccessfully: Bool
+    ) {
+        self.connectionStateManager = connectionStateManager
+        self.mockDeviceInfo = mockDeviceInfo
+        self.connectsSuccessfully = connectsSuccessfully
+        self.sendsRequestSuccessfully = sendsRequestSuccessfully
+    }
+    
+    func connect() async throws {
+        await connectionStateManager.connect {
+            try await Task.longSleep()
+            guard !connectsSuccessfully else { return }
+            
+            throw ConnectionError()
+        }
+    }
+    
+    func disconnect() async throws {
+        await connectionStateManager.disconnect {
+            try await Task.mediumSleep()
+        }
+    }
+    
+    func send<T: Decodable>(_ request: DeviceRequest) async throws -> T {
+        let endpoint = request.endpoint
+        let data: Data
+        
+        switch SupportedEndpoint(endpoint: endpoint) {
+        case .deviceInfo: data = try JSONEncoder().encode(mockDeviceInfo)
+        case .none: throw HandlingUnsupportedEndpointError(endpoint: endpoint)
+        }
+        
+        return try await response(data)
+    }
+    
+    private func response<T: Decodable>(_ data: Data) async throws -> T {
+        if sendsRequestSuccessfully {
+            try await Task.veryLongSleep()
+            return try JSONDecoder().decode(T.self, from: data)
+        } else {
+            throw SendingRequestError()
+        }
+    }
+}
+
+extension WiFiTransport {
+    typealias ConnectionStateStream = MulticastAsyncStream<ConnectionState>
+    typealias GetConnectionStateManager = (ConnectionStateStream) -> AnyDeviceTransportConnectionStateManager
+    
+    static func create(
+        mockDeviceInfo: DeviceInfo,
+        connectsSuccessfully: Bool = .random(),
+        sendsRequestSuccessfully: Bool = .random(),
+        getConnectionStateManager: GetConnectionStateManager = DeviceTransportConnectionStateManager.init
+    ) -> AnyDeviceTransport {
+        let connectionStateStream = ConnectionStateStream()
+        return WiFiTransport(
+            connectionStateManager: getConnectionStateManager(connectionStateStream),
+            mockDeviceInfo: mockDeviceInfo,
+            connectsSuccessfully: connectsSuccessfully,
+            sendsRequestSuccessfully: sendsRequestSuccessfully
+        )
+        .withSendingCancellationWhenNotConnected()
+        .withSendingErrorWhenNotConnected()
+    }
+}
